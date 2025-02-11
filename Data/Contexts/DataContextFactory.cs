9da02fb451f08d362793c2e57f91db7063966b3b
Joakim N <92833271+Echoinstrum@users.noreply.github.com>
2025-02-11 11:12:07 +0100

    Database, entities and tables setup

---
+++ Data/Contexts/DataContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Contexts;
+
+public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+{
+    public DataContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+        optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Workspace\\Databaskurs\\Database_Course_Assignment\\Data\\Databases\\local_db.mdf;Integrated Security=True;Connect Timeout=30");
+
+        return new DataContext(optionsBuilder.Options);
+    }
+}
+
---
+++ Data/Entities/StatusTypeEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Data.Entities;
+
+public class StatusTypeEntity
+{
+    public int Id { get; set; }
+    public string StatusName { get; set; } = null!;
+}
